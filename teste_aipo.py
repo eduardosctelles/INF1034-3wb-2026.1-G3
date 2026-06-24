98b8f36a555325a405e73b2b2c92351405650857
TheoVL852 <theovisconti@icloud.com>
2026-06-24 14:54:47 -0300

    aipo list

--- teste_aipo.py
+++ teste_aipo.py
@@ -7,14 +7,13 @@ clock = pygame.time.Clock()
 animation_aipo = True
 
 posicaoSheet_aipo = 0
-pos_x_aipo = 700
-pos_y_aipo = 200
 curr_frame_aipo = 0
 anim_time_aipo = 0
 spritesheet_aipo = pygame.image.load('AEvilery-Sheet.png')
 flip_aipo = pygame.transform.flip(spritesheet_aipo,True,False)
 aipo = flip_aipo
 collider_inimigos = []
+aipos = [{"x": 700, "y": 200}]
 
 
 pygame.init()
@@ -26,16 +25,17 @@ while True:
             pygame.quit()
             sys.exit()
 
-    pos_x_aipo -= 3
+    for aipo_info in aipos:
+        aipo_info["x"] -= 3
 
-    if pos_x_aipo <= -32:
-        pos_x_aipo = 732
+    aipos = [a for a in aipos if a["x"] > -32]        
 
-            
+    if len(aipos) == 0 or aipos[-1]["x"] < 300:
+        aipos.append({"x": 700,"y": 200})
+
     clock.tick(60)
     dt = clock.get_time()
     
-    collider_aipo = pygame.Rect(pos_x_aipo,pos_y_aipo,32,32)
 
     if animation_aipo :
         posicaoSheet_aipo = 0
@@ -50,7 +50,12 @@ while True:
           
     #Desenho dos elementos 
     screen.fill((255,255,255))
-    pygame.draw.rect(screen, (0, 0, 255), collider_aipo, 2)
-    screen.blit(aipo,(pos_x_aipo,pos_y_aipo),(32*(curr_frame_aipo%13)+64,posicaoSheet_aipo,32,32))
+    for aipo_info in aipos: 
+
+        collider_aipo = pygame.Rect(aipo_info["x"],aipo_info["y"],32,32)
+
+        pygame.draw.rect(screen,(0, 0, 255),collider_aipo,2)
+
+        screen.blit(aipo,(aipo_info["x"], aipo_info["y"]),(32 * (curr_frame_aipo % 13) + 64,posicaoSheet_aipo,32,32))
 
     pygame.display.update()
